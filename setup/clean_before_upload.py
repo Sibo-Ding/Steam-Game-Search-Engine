0e33a9dcc89216966954b762dcadcd7b0d61078a
Sibo Ding <siboding00@gmail.com>
2024-11-25 23:44:22 -0600

    Add sentence transformer
    Use Google Colab to run .ipynb

--- setup/clean_before_upload.py
+++ setup/clean_before_upload.py
@@ -1,7 +1,9 @@
 # Save steam_data.csv under "data" folder, because it is too big to upload to GitHub.
 
 import os
+from tqdm import tqdm
 import pandas as pd
+from sentence_transformers import SentenceTransformer
 
 # Change working directory to the current .py file
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -9,19 +11,44 @@ os.chdir(current_directory)
 # Change working directory to "data"
 os.chdir("../data")
 
-# Read csv + Drop NA Title + Clean price + Clean date + Select columns + Write csv
-df = pd.read_csv("steam_data.csv")
-df.dropna(
-    subset=["Title"]
-).assign(
-    original_price=df["Original Price"]
-    .replace("Free", "0")  # Replace "Free" with "0"
-    .str.replace(r"[$,]", "", regex=True)  # Remove "$" and ","
-    .astype(float)  # Convert to float
-).assign(
-    # If a date is not in "Jan 1, 2000" format, set it to NaT/NaN
-    release_date=pd.to_datetime(df["Release Date"], format="%d %b, %Y", errors="coerce")
-)[
+tqdm.pandas()  # Progress bar
+
+# Read csv + Drop NA Title
+# Clean price: Replace "Free" with "0" + Remove "$" and "," + Convert to float
+# Clean date: coerce: If a date is not in "Jan 1, 2000" format, set it to NaT/NaN
+# Combine Title, Description, Tags, and Features to one column "search_text"
+df = (
+    pd.read_csv("steam_data.csv")
+    .dropna(subset=["Title"])
+    .assign(
+        original_price=lambda df_: df_["Original Price"]
+            .replace("Free", "0")
+            .str.replace(r"[$,]", "", regex=True)
+            .astype(float),
+        release_date=lambda df_: pd.to_datetime(
+            df_["Release Date"], format="%d %b, %Y", errors="coerce"
+        ),
+        search_text=lambda df_: df_["Title"]
+            + " " + df_["Game Description"]
+            + " " + df_["Popular Tags"]
+            + " " + df_["Game Features"],
+    )
+)
+
+# Initialize Sentence Transformer model
+model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+# Other more computationally complex models
+# model = SentenceTransformer("distiluse-base-multilingual-cased-v1")
+# model = SentenceTransformer("xlm-r-bert-base-nli-stsb-mean-tokens")
+# model = SentenceTransformer("LaBSE")
+
+# Add embedding column with progress bar
+df["embedding"] = df["search_text"].progress_apply(
+    lambda text: model.encode(text).tolist()
+)
+
+# Select columns + Write csv
+df[
     [
         "Title",
         "original_price",
@@ -32,5 +59,6 @@ df.dropna(
         "Supported Languages",
         "Popular Tags",
         "Game Features",
+        "embedding",
     ]
 ].to_csv("steam_clean_no_header.csv", header=False, index=False)
