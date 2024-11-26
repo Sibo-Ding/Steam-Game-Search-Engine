6f8e652d2bc82c7125326a7ce432104070f0e89a
Sibo Ding <siboding00@gmail.com>
2024-11-26 16:55:05 -0600

    Update create_tables.sql
    Add vector embeddings

--- setup/create_tables.sql
+++ setup/create_tables.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
 CREATE TABLE steam (
     name VARCHAR(250) PRIMARY KEY,
     original_price NUMERIC(10, 2),
@@ -7,5 +9,6 @@ CREATE TABLE steam (
     developer VARCHAR(1000),
     supported_languages VARCHAR(1500),
     tags VARCHAR(500),
-    features VARCHAR(500)
+    features VARCHAR(500),
+    embeddings VECTOR(384)
 );
