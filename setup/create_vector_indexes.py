3fc1bd8866535a140e070e02e13fddaa67fe6afd
Sibo Ding <siboding00@gmail.com>
2024-11-26 21:02:02 -0600

    Update create_vector_indexes.py
    Format

--- setup/create_vector_indexes.py
+++ setup/create_vector_indexes.py
@@ -1,6 +1,7 @@
 # Adapted from Google Cloud pgvector demo
-# https://cloud.google.com/blog/products/databases/faster-similarity-search-performance-with-pgvector-indexes
+# https://cloud.google.com/blog/products/databases/using-pgvector-llms-and-langchain-with-google-cloud-databases
 # https://colab.research.google.com/github/GoogleCloudPlatform/python-docs-samples/blob/main/cloud-sql/postgres/pgvector/notebooks/pgvector_gen_ai_demo.ipynb#scrollTo=_zRBR9YJoENp
+# https://cloud.google.com/blog/products/databases/faster-similarity-search-performance-with-pgvector-indexes
 
 from database import engine
 
@@ -13,7 +14,7 @@ operator = "vector_cosine_ops"  # @param ["vector_cosine_ops", "vector_l2_ops", 
 with engine.begin() as conn:
     conn.exec_driver_sql(
         f"""CREATE INDEX ON steam
-              USING hnsw(embeddings {operator})
-              WITH (m = {m}, ef_construction = {ef_construction})
+            USING hnsw(embeddings {operator})
+            WITH (m = {m}, ef_construction = {ef_construction})
             """
     )
